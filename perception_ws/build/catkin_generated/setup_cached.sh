12e8e3537e1b96473f194489e754b771a79e3721
jerry <qilong.cheng@mail.utoronto.ca>
2024-05-29 16:35:37 -0400

    new workspace

---
+++ perception_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export PWD='/home/jerry/perception_ws/build'
+export PYTHONPATH="/home/jerry/perception_ws/devel/lib/python3/dist-packages:$PYTHONPATH"
+export ROS_PACKAGE_PATH='/home/jerry/perception_ws/src:/opt/ros/noetic/share'
---
+++ perception_ws/build/realsense-ros/realsense2_camera/CMakeFiles/realsense2_camera_generate_messages_eus.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "/home/jerry/perception_ws/devel/share/roseus/ros/realsense2_camera/manifest.l"
+  "/home/jerry/perception_ws/devel/share/roseus/ros/realsense2_camera/msg/Extrinsics.l"
+  "/home/jerry/perception_ws/devel/share/roseus/ros/realsense2_camera/msg/IMUInfo.l"
+  "/home/jerry/perception_ws/devel/share/roseus/ros/realsense2_camera/msg/Metadata.l"
+  "/home/jerry/perception_ws/devel/share/roseus/ros/realsense2_camera/srv/DeviceInfo.l"
+  "CMakeFiles/realsense2_camera_generate_messages_eus"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/realsense2_camera_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
